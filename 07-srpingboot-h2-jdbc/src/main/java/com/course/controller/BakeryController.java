7bd36c8fe43887d6735c65f3767e4f33dc065a54
CodingFarmer888 <152504151+CodingFarmer888@users.noreply.github.com>
2024-01-23 23:23:33 +0800

    Bakery Project

--- 07-srpingboot-h2-jdbc/src/main/java/com/course/controller/BakeryController.java
+++ 07-srpingboot-h2-jdbc/src/main/java/com/course/controller/BakeryController.java
@@ -4,35 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.course.entity.Product;
 import com.course.service.ProductService;
 
 @CrossOrigin(value = "http://localhost:3000")
-@Controller
+@RestController
 public class BakeryController {
 	
 	@Autowired
 	private ProductService service;
 	
 	@PostMapping("/add")
-	@ResponseBody
 	public String addProduct(String name, String descript, String img, Integer price, String category) {
 		service.addProduct(name, descript, img, price, category);
 		return "add";
 	}
 	
 	@GetMapping("/products")
-	@ResponseBody
 	public ResponseEntity<List<Product>> getAllProduct() {
 		List<Product> productList = service.findAllProduct();
 		return ResponseEntity.ok(productList);
@@ -40,7 +36,6 @@ public class BakeryController {
 	
 	
 	@GetMapping("/product/{id}")
-	@ResponseBody
 	public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
 		Product product = service.findById(id);
 		return ResponseEntity.ok(product);
@@ -48,42 +43,15 @@ public class BakeryController {
 	}
 	
 	@PutMapping("/update")
-	@ResponseBody
 	public String addProduct(Long id, String name, String descript, String img, Integer price, String category) {
 		service.update(id, name, descript, img, price, category);
 		return "update";
 	}
 	
 	@DeleteMapping("/delete/{id}")
-	@ResponseBody
 	public String deleteProduct(@PathVariable Long id) {
 		service.deleteById(id);
 		return "delete";
-	}
-	
-	
-	/** ------導頁Controller------ */
-	
-	@GetMapping(value = {"/"})
-	public ModelAndView index() {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("index");
-		return mav;
-	}
-	
-	@GetMapping("/about")
-	public String toAboutPage() {
-		return "about";
-	}
-	
-	@GetMapping("/detail")
-	public String toDetailPage() {
-		return "detail";
-	}
-	
-	@GetMapping("/product")
-	public String toProductPage() {
-		return "product";
 	}
 
 }

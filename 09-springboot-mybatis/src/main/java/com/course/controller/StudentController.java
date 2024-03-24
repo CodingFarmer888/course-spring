9a34cc134eada51a5a8f0320e279a979ac0ae5c8
CodingFarmer888 <152504151+CodingFarmer888@users.noreply.github.com>
2024-03-24 14:11:03 +0800

    MyBatis

--- 09-springboot-mybatis/src/main/java/com/course/controller/StudentController.java
+++ 09-springboot-mybatis/src/main/java/com/course/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.course.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.course.mapper.StudentMapper;
@@ -44,5 +46,33 @@ public class StudentController {
 	@DeleteMapping("/student/{id}")
 	public void updateStudent(@PathVariable Long id) {
 		studentMapper.deleteStudent(id);
+	}
+	
+	@GetMapping("/students/findByIf")
+	public List<StudentVo> findByIf(@RequestParam(required = false) String name, @RequestParam(required = false) String sex) {
+		String searchName = StringUtils.isNotBlank(name) ? "%" + name + "%" : StringUtils.EMPTY;
+		return studentMapper.findByIf(searchName, sex);
+	}
+	
+	@GetMapping("/students/findByChoose")
+	public List<StudentVo> findByChoose(@RequestParam(required = false) String name, @RequestParam(required = false) String sex) {
+		String searchName = StringUtils.isNotBlank(name) ? "%" + name + "%" : StringUtils.EMPTY;
+		return studentMapper.findByChoose(searchName, sex);
+	}
+	
+	@GetMapping("/students/findByWhere")
+	public List<StudentVo> findByWhere(@RequestParam(required = false) String name, @RequestParam(required = false) String sex) {
+		String searchName = StringUtils.isNotBlank(name) ? "%" + name + "%" : StringUtils.EMPTY;
+		return studentMapper.findByWhere(searchName, sex);
+	}
+	
+	@GetMapping("/students/findByNames")
+	public List<StudentVo> findByNames(@RequestParam List<String> names) {
+		return studentMapper.findByNames(names);
+	}
+	
+	@PutMapping("/student/updateStudentByCondition")
+	public void updateStudentByCondition(@RequestBody StudentVo student) {
+		studentMapper.updateStudentByCondition(student);
 	}
 }
--- 09-springboot-mybatis/src/main/java/com/course/mapper/StudentMapper.java
+++ 09-springboot-mybatis/src/main/java/com/course/mapper/StudentMapper.java
@@ -1,5 +1,6 @@
 package com.course.mapper;
 
+import java.util.Date;
 import java.util.List;
 
 import com.course.vo.StudentVo;
@@ -15,5 +16,17 @@ public interface StudentMapper {
 	void updateStudent(StudentVo student);
 	
 	void deleteStudent(Long id);
+	
+	List<StudentVo> findByIf(String name, String sex);
+	
+	List<StudentVo> findByChoose(String name, String sex);
+	
+	List<StudentVo> findByWhere(String name, String sex);
+	
+	List<StudentVo> findByNames(List<String> names);
+	
+	void updateStudentByCondition(StudentVo student);
+
+	List<StudentVo> findByName(String name, String sex, Date startDate, Date endDate);
 	
 }

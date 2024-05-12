651fc9827251d6dd48214fd993e29f1135bb173b
CodingFarmer888 <152504151+CodingFarmer888@users.noreply.github.com>
2024-05-12 23:22:06 +0800

    Logger

--- 04-springboot-hello/src/main/java/com/course/Application.java
+++ 04-springboot-hello/src/main/java/com/course/Application.java
@@ -1,5 +1,7 @@
 package com.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +19,16 @@ public class Application {
 		
 		BenzCar benz = ctx.getBean(BenzCar.class);
 		benz.move();
+		
+		// Logger
+		System.out.println("============= Logger ===============");
+		
+		Logger logger = LoggerFactory.getLogger(Application.class);
+		logger.trace("我是trace");
+		logger.debug("我是debug");
+		logger.info("我是info");
+		logger.warn("我是warn");
+		logger.error("我是error");
 	}
 
 }

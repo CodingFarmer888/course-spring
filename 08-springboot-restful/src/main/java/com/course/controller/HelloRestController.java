04f4b7c713416973fef08651fd82bc17d9ceebbe
CodingFarmer888 <152504151+CodingFarmer888@users.noreply.github.com>
2024-06-09 23:58:51 +0800

    Cors

--- 08-springboot-restful/src/main/java/com/course/controller/HelloRestController.java
+++ 08-springboot-restful/src/main/java/com/course/controller/HelloRestController.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,7 @@ import com.course.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
+@CrossOrigin(origins ={"http://127.0.0.1:5500"}, allowedHeaders = "*", allowCredentials = "true")
 public class HelloRestController {
 	
 	@Autowired
@@ -44,6 +47,7 @@ public class HelloRestController {
 
 	@Operation(summary = "取得所有使用者", description = "取得所有使用者", tags = "Annotation 介紹")
 	@GetMapping("/users")
+	// @CrossOrigin(origins = "http://127.0.0.1:5500", methods = {RequestMethod.GET})
 	public List<User> getAllUsers() {
 		return userService.queryAllUsers();
 	}
